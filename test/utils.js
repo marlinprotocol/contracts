3965784534de7cd81e105e934a4fa776027356b3
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-25 14:26:31 +0530

    Add ESLint AirBnB style and lint

--- test/utils.js
+++ test/utils.js
@@ -1,21 +1,21 @@
-var Utils = function () {};
+const Utils = function base() {};
 
-Utils.prototype.arrToHexString = function (arr) {
-  var hexStr = '';
-  for (var i = 0; i < arr.length; i++) {
-    var hex = arr[i].toString(16);
-    hex = (hex.length === 1) ? '0' + hex : hex;
+Utils.prototype.arrToHexString = (arr) => {
+  let hexStr = '';
+  for (let i = 0; i < arr.length; i += 1) {
+    let hex = arr[i].toString(16);
+    hex = (hex.length === 1) ? `0${hex}` : hex;
     hexStr += hex;
   }
   return hexStr.toLowerCase();
-}
+};
 
-Utils.prototype.hexStringToArr = function (hex) {
-  var arr = [];
-  for (var i = 0; i < hex.length; i+=2) {
-    arr.push(parseInt(hex.substr(i,2),16));
+Utils.prototype.hexStringToArr = (hex) => {
+  const arr = [];
+  for (let i = 0; i < hex.length; i += 2) {
+    arr.push(parseInt(hex.substr(i, 2), 16));
   }
   return new Uint8Array(arr);
-}
+};
 
 module.exports = new Utils();
