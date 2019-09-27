9587aeb7a96ce903b7d2dcbde6f18594d4162731
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-27 23:50:47 +0530

    Fix failing test as a result of fixing signature verification

--- test/crypto.js
+++ test/crypto.js
@@ -11,6 +11,7 @@ const utils = require('./utils.js');
 
 const privateKey = utils.hexStringToArr('360970a734fe426d73f06224de95aca35a88031bd5d7851c7b340ecdb5a94f84');
 const publicKey = utils.hexStringToArr('f1cb00a966c77dcc6d2c59c8f5181a4a68ed374e8b0412b08e1ee7a81059b7e34e419719eff5c26095a6c34afd936368ae2af7a5e85fcefd1201e2a72497ca48');
+const address = utils.hexStringToArr('fD82f9A61b3c600B8B83b8ADDBEc35D5C27C4105');
 const testMsg = Uint8Array.from('test message');
 const testSignature = utils.hexStringToArr('cf029ecf49a1b14f7abb8322dd027298d6521acfdf357ca9f221ad5191fbf5466efbea552ac33cfd90784c2a01cfc8174d48c47c09b6df4c3cb3b67f38d52a7c1c');
 
@@ -46,7 +47,7 @@ describe('crypto', () => {
 describe('crypto', () => {
   describe('verify', () => {
     it('should verify signature with test data', () => {
-      const isVerified = crypto.verify(testMsg, testSignature, publicKey);
+      const isVerified = crypto.verify(testMsg, testSignature, address);
       expect(isVerified).to.be.true; // eslint-disable-line no-unused-expressions
     });
   });
