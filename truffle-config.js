59ed9cd341aa25e5f6db56b554630fa1041d3b91
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-21 17:28:08 +0530

    Enable optimization

--- truffle-config.js
+++ truffle-config.js
@@ -43,9 +43,9 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7693,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7693,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
 
     // Another network with more advanced options...
@@ -94,13 +94,12 @@ module.exports = {
     solc: {
       version: "0.6.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 1000000
+        },
+      }
     }
   }
 }
