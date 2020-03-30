2c9c0134305a785dd5903e33c52232a6d285337e
Akshay Meher <akshay_111meher@hotmail.com>
2020-03-31 03:00:21 +0530

    Dockerfile added, Private network enabled

--- truffle-config.js
+++ truffle-config.js
@@ -17,6 +17,9 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+const privateKey = "4b306d0ee52f310b23aafd093298b896755ac389fa169d99378f9becac5a6e71";
+// Address of the above private key: 0x0AE167943B6d5bD1a1763Cb64d014a08c4125FA1
 
 // const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
@@ -64,7 +67,7 @@ module.exports = {
       from: "0xbb434d361c5ff56876826ac92e9eda834334aef5", // default address to use for any transaction Truffle makes during migrations
       network_id: 4,
       gas: 4612388 // Gas limit used for deploys
-    }
+    },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
@@ -77,11 +80,16 @@ module.exports = {
     // },
 
     // Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    private: {
+      host: "68.183.87.16",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 4700000,
+      gasPrice: 10000000000,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      provider: () => new PrivateKeyProvider(privateKey, "http://68.183.87.16:8545")
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
