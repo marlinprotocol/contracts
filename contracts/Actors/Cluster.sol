c6f656201cb22395f52525ee377bf65afc9bc9fb
Prateek Reddy <prateekreddyy@gmail.com>
2020-08-23 12:20:10 +0530

    merge

--- contracts/Actors/Cluster.sol
+++ contracts/Actors/Cluster.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: <SPDX-License>
 
-<<<<<<< HEAD
 pragma solidity ^0.7.0;
 
 contract Cluster {
@@ -8,10 +7,4 @@ contract Cluster {
     function isReceiver(address _receiver) public {
 
     }
-=======
-pragma solidity >=0.4.21 <0.7.0;
-
-contract Cluster {
-    
->>>>>>> 536fcda5683e861cbb8c2c926af726859085c847
 }
