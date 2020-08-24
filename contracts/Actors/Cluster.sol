5114d79a7a133c10c2a740a5c66fc4356a86f166
Prateek Reddy <prateekreddyy@gmail.com>
2020-08-24 11:37:24 +0530

    clean up

--- contracts/Actors/Cluster.sol
+++ contracts/Actors/Cluster.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: <SPDX-License>
 
-pragma solidity ^0.7.0;
+pragma solidity >=0.4.21 <0.7.0;
 
 contract Cluster {
     // TODO: make this an interface where only felow function is necessary
