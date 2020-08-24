02557aaa227f2b7ca79956e21d32acb6529483c3
Prateek Reddy <prateekreddyy@gmail.com>
2020-08-24 09:28:29 +0530

    Actors: Producer contract done

--- contracts/Actors/Producer.sol
+++ contracts/Actors/Producer.sol
@@ -2,6 +2,18 @@
 
 pragma solidity >=0.4.21 <0.7.0;
 
+uint endBlock;
+
+// Note: This contract is chain specific as signature has to be verified
 contract Producer {
-    
+
+    mapping(address => bool) producers;
+
+    function join(address _producer) public {
+        producers[_producer] = true;
+    }
+
+    function isProducer(address _producer) public returns(bool) {
+        return producers[_producer];
+    }
 }
