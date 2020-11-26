a12d96924b866031e3778c548134a63a7e6ec081
akshay111meher <akshay_111meher@hotmail.com>
2020-11-26 12:53:20 +0530

    Temp

--- test/Mpond.js
+++ test/Mpond.js
@@ -1,4 +1,5 @@
 const { Contract } = require("ethers");
+const { it } = require("ethers/wordlists");
 
 const mPondProxy = artifacts.require("mPondProxy.sol");
 const mPondLogic = artifacts.require("mPondLogic.sol");
@@ -6,5 +7,7 @@ const mPondLogic = artifacts.require("mPondLogic.sol");
 var mPondInstance;
 
 contract.only("MPond Contract", function(accounts){
-    
+    it("Deploy contracts", function(){
+        return mPondProxy.new(mPondLogic.address, {from: accounts[1]});
+    })
 })
