1aa0b63944e3e6d38741b84d8779ef3906c80362
Prateek Reddy <prateekreddyy@gmail.com>
2020-11-26 20:56:18 +0530

    delegator and cluster reward distribution tested

--- contracts/Stake/PerfOracle.sol
+++ contracts/Stake/PerfOracle.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.4.21 <0.7.0;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
+import "./ClusterRegistry.sol";
 
 contract PerfOracle is Ownable {
 
@@ -14,64 +15,80 @@ contract PerfOracle is Ownable {
 
     mapping(address => ClusterData) clusters;
 
-    uint256 runningFeederEpochWeight;
+    uint256 public runningFeederEpochWeight;
     uint256 rewardPerWeight;
 
     uint256 rewardPerEpoch;
-    address clusterRegistry;
+    address clusterRegistryAddress;
     ERC20 MPOND;
 
-    uint256 currentEpoch;
+    uint256 public currentEpoch;
 
     bool feedInProgress;
 
     modifier onlyClusterRegistry() {
-        require(msg.sender == clusterRegistry);
+        require(msg.sender == clusterRegistryAddress);
         _;
     }
 
-    constructor(address _owner, address _clusterRegistry, uint256 _rewardPerEpoch, address _MPONDAddress) public Ownable() {
-        // transferOwnership(_owner);
-        clusterRegistry = _clusterRegistry;
+    constructor(address _owner, address _clusterRegistryAddress, uint256 _rewardPerEpoch, address _MPONDAddress) public Ownable() {
+        initialize(_owner);
+        clusterRegistryAddress = _clusterRegistryAddress;
         rewardPerEpoch = _rewardPerEpoch;
         MPOND = ERC20(_MPONDAddress);
     }
 
     function feed(uint _epoch, address[] memory _clusters, uint256[] memory _perf) public onlyOwner {
-        require(_epoch == currentEpoch+1);
+        require(_epoch == currentEpoch+1, "PerfOracle:feed - Invalid Epoch");
         for(uint256 i=0; i < _clusters.length; i++) {
-            uint256 weight = getEffectiveStake(_clusters[i])*_perf[i];
+            uint256 weight = ClusterRegistry(clusterRegistryAddress).getEffectiveStake(_clusters[i])*_perf[i];
             clusters[_clusters[i]].unrewardedWeight = weight;
             runningFeederEpochWeight += weight;
         }
-        feedInProgress = true;
+        if(runningFeederEpochWeight != 0) {
+            feedInProgress = true;
+        } 
     }
 
     function closeFeed(uint _epoch) public onlyOwner {
-        require(_epoch == currentEpoch+1);
+        require(
+            _epoch == currentEpoch+1 
+            && feedInProgress,  
+            "PerfOracle:closeFeed - Invalid epoch or valid feed not provided"
+        );
         rewardPerWeight = rewardPerEpoch*10**30/runningFeederEpochWeight;
         delete runningFeederEpochWeight;
     }
 
     function distributeRewards(uint _epoch, address[] memory _clusters) public onlyOwner {
-        require(_epoch == currentEpoch+1);
+        require(
+            _epoch == currentEpoch+1 
+            && feedInProgress 
+            && runningFeederEpochWeight == 0,
+            "PerfOracle:distributeRewards - Invalid epoch or feed not closed"
+        );
         for(uint256 i=0; i < _clusters.length; i++) {
             clusters[_clusters[i]].rewards += clusters[_clusters[i]].unrewardedWeight*rewardPerWeight/10**30;
         }
     }
 
     function lockEpoch(uint _epoch) public onlyOwner {
-        require(_epoch == currentEpoch+1);
+        require(
+            _epoch == currentEpoch+1 
+            && feedInProgress 
+            && runningFeederEpochWeight == 0,
+            "PerfOracle:lockEpoch - Invalid epoch or feed not closed"
+        );
         feedInProgress = false;
         delete rewardPerWeight;
         currentEpoch = _epoch;
     }
 
     function claimReward(address _cluster) public onlyClusterRegistry returns(uint256) {
-        require(!feedInProgress);
+        require(!feedInProgress, "PerfOracle:claimReward - Feed in progress");
         uint256 pendingRewards = clusters[_cluster].rewards;
         if(pendingRewards > 0) {
-            transferRewards(clusterRegistry, clusters[_cluster].rewards);
+            transferRewards(clusterRegistryAddress, clusters[_cluster].rewards);
             delete clusters[_cluster].rewards;
         }
         return pendingRewards;
@@ -87,9 +104,5 @@ contract PerfOracle is Ownable {
 
     function transferRewards(address _to, uint256 _amount) internal {
         MPOND.transfer(_to, _amount);
-    }
-
-    function getEffectiveStake(address _cluster) public returns(uint256) {
-
     }
 }
--- test/ClusterRegistry-StakingMVP.js
+++ test/ClusterRegistry-StakingMVP.js
@@ -14,7 +14,6 @@ const { BigNumber } = require("ethers/utils");
 const appConfig = require("../app-config");
 const truffleAssert = require("truffle-assertions");
 const { AddressZero } = require("ethers/constants");
-const { it } = require("ethers/wordlists");
 
 contract("Stake contract", async function(accounts) {
 
@@ -33,6 +32,15 @@ contract("Stake contract", async function(accounts) {
     const unregisteredClusterRewardAddress = accounts[8];
     const deregisteredCluster = accounts[6];
     const deregisteredClusterRewardAddress = accounts[9];
+    const registeredCluster1 = accounts[11];
+    const registeredCluster2 = accounts[12];
+    const registeredCluster3 = accounts[13];
+    const registeredCluster4 = accounts[14];
+    const delegator1 = accounts[15];
+    const delegator2 = accounts[16];
+    const delegator3 = accounts[17];
+    const delegator4 = accounts[18];
+    const clientKey = accounts[19];
 
     it("deploy stake contract and initialize tokens and whitelist stake contract", async () => {
         const PONDDeployment = await PONDToken.new();
@@ -71,12 +79,17 @@ contract("Stake contract", async function(accounts) {
         const perfOracleAddress = await clusterRegistry.oracle();
         perfOracle = await PerfOracle.at(perfOracleAddress);
 
-        console.log((await perfOracle.owner()));
+        assert((await perfOracle.owner()) == oracleOwner, "Owner not correctly set");
 
         await MPONDInstance.addWhiteListAddress(stakeContract.address, {
             from: admin
         })
         assert((await MPONDInstance.isWhiteListed(stakeContract.address), "StakeManager contract not whitelisted"));
+
+        await MPONDInstance.addWhiteListAddress(clusterRegistry.address, {
+            from: admin
+        })
+        assert((await MPONDInstance.isWhiteListed(clusterRegistry.address), "clusterRegistry contract not whitelisted"));
 
         await MPONDInstance.addWhiteListAddress(perfOracle.address, {
             from: admin
@@ -86,6 +99,8 @@ contract("Stake contract", async function(accounts) {
         await MPONDInstance.transfer(perfOracle.address, appConfig.staking.rewardPerEpoch*100, {
             from: MPONDAccount
         });
+
+        await PONDInstance.mint(accounts[0], new BigNumber("100000000000000000000"));
     });
 
     it("Register cluster", async () => {
@@ -163,4 +178,92 @@ contract("Stake contract", async function(accounts) {
     it("check if cluster is active", async () => {
 
     });
+
+    it("Delegator withdraw rewards from a cluster", async () => {
+        await clusterRegistry.register(10, registeredClusterRewardAddress, clientKey, {
+            from: registeredCluster1
+        });
+        await clusterRegistry.register(5, registeredClusterRewardAddress, clientKey, {
+            from: registeredCluster2
+        });
+        // 2 users delegate tokens to a cluster - one twice the other
+        await delegate(delegator1, [registeredCluster1, registeredCluster2], [1000000, 2], [0, 1]);
+        await delegate(delegator2, [registeredCluster1, registeredCluster2], [5000000, 1], [0, 1]);
+        // data is fed to the oracle
+        const epochToFeed = parseInt((await perfOracle.currentEpoch())) + 1;
+        await feedData(epochToFeed, [registeredCluster, registeredCluster1, registeredCluster2, registeredCluster3, registeredCluster4]);
+        // do some delegations for both users to the cluster
+        // rewards for one user is withdraw - this reward should be as per the time of oracle feed
+        let MPondBalance1Before = await MPONDInstance.balanceOf(delegator1);
+        await delegate(delegator1, [registeredCluster1, registeredCluster2], [1000000, 2], [0, 1]);
+        let MPondBalance1After = await MPONDInstance.balanceOf(delegator1);
+        console.log(MPondBalance1After.sub(MPondBalance1Before).toString(), appConfig.staking.rewardPerEpoch/3);
+        assert(MPondBalance1After.sub(MPondBalance1Before).toString() == parseInt(appConfig.staking.rewardPerEpoch*(2.0/3*9/10*1/6+1.0/3*19/20*2/3)));
+        // feed data again to the oracle
+        console.log((await perfOracle.currentEpoch()), epochToFeed+1);
+        await feedData(epochToFeed+1, [registeredCluster, registeredCluster1, registeredCluster2, registeredCluster3, registeredCluster4]);
+        // do some delegations for both users to the cluster
+        let MPondBalance2Before = await MPONDInstance.balanceOf(delegator2);
+        await delegate(delegator2, [registeredCluster1, registeredCluster2], [1000000, 2], [0, 1]);
+        let MPondBalance2After = await MPONDInstance.balanceOf(delegator2);
+        console.log(MPondBalance2After.sub(MPondBalance2Before).toString(), appConfig.staking.rewardPerEpoch*((2.0/3*9/10*5/6+1.0/3*19/20*1/3)+(7.0/12*9/10*5/7+5.0/12*19/20*1/5)));
+        assert(MPondBalance2After.sub(MPondBalance2Before).toString() == parseInt(appConfig.staking.rewardPerEpoch*((2.0/3*9/10*5/6+1.0/3*19/20*1/3)+(7.0/12*9/10*5/7+5.0/12*19/20*1/5))));
+    });
+
+    async function delegate(delegator, clusters, amounts, tokenType) {
+        let totalPond = 0;
+        let totalMPond = 0;
+        for(let i=0; i < amounts.length; i++) {
+            if(tokenType[i] == 0) {
+                totalPond += amounts[i];
+            } else if(tokenType[i] == 1) {
+                totalMPond += amounts[i];
+            }     
+        }
+
+        if(totalMPond > 0) {
+            await PONDInstance.transfer(delegator, totalPond);
+            await PONDInstance.approve(stakeContract.address, totalPond, {
+                from: delegator
+            });
+        }
+        if(totalMPond > 0) {
+            await MPONDInstance.transfer(delegator, totalMPond, {
+                from: MPONDAccount
+            });
+            await MPONDInstance.approve(stakeContract.address, totalMPond, {
+                from: delegator
+            });
+        }
+        const stashes = [];
+        for(let i=0; i < clusters.length; i++) {
+            console.log(tokenType[i], amounts[i], clusters[i]);
+            const receipt = await stakeContract.createStashAndDelegate(tokenType[i], amounts[i], clusters[i], {
+                from: delegator
+            });
+            stashes.push(receipt.logs[0].args.stashId);
+        }
+        return stashes;
+    }
+
+    async function feedData(epoch, clusters) {
+        const perf = [];
+        for(let i=0; i < clusters.length; i++) {
+            perf.push(100);
+            // perf.push(parseInt(100*Math.random()));
+        }
+
+        await perfOracle.feed(epoch, clusters, perf, {
+            from: oracleOwner
+        });
+        await perfOracle.closeFeed(epoch, {
+            from: oracleOwner
+        });
+        await perfOracle.distributeRewards(epoch, clusters, {
+            from: oracleOwner
+        });
+        await perfOracle.lockEpoch(epoch, {
+            from: oracleOwner
+        });
+    }
 });
