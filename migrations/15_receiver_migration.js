2645422d857a95311da7c801662f0056d7ef48cc
Prateek Reddy <prateekreddyy@gmail.com>
2020-12-06 16:23:28 +0530

    add migrations

---
+++ migrations/15_receiver_migration.js
@@ -0,0 +1,7 @@
+let Receiver = artifacts.require("Receiver.sol");
+
+module.exports = async function (deployer, network, accounts) {
+  if (network == "development") {
+    //todo: how to initalize
+  }
+};
