652b19aa764d639a0da7285d7ba5db8a1ae2dfe6
Prateek Reddy <prateekreddyy@gmail.com>
2020-12-13 12:52:02 +0530

    Rewards per network added

--- contracts/Stake/ClusterRewards.sol
+++ contracts/Stake/ClusterRewards.sol
@@ -12,13 +12,18 @@ contract ClusterRewards is Initializable, Ownable {
 
     mapping(address => uint256) public clusterRewards;
 
-    uint256 rewardPerEpoch;
+    mapping(bytes32 => uint256) public rewardWeight;
+    uint256 totalWeight;
+    uint256 totalRewardsPerEpoch;
     uint256 payoutDenomination;
 
     address rewardDelegatorsAddress;
     ERC20 POND;
 
-    uint256 public currentEpoch;
+    event NetworkAdded(bytes32 networkId, uint256 rewardPerEpoch);
+    event NetworkRemoved(bytes32 networkId);
+    event NetworkRewardUpdated(bytes32 networkId, uint256 updatedRewardPerEpoch);
+    event ClusterRewarded(bytes32 networkId);
 
     modifier onlyRewardDelegatorsContract() {
         require(msg.sender == rewardDelegatorsAddress);
@@ -28,25 +33,70 @@ contract ClusterRewards is Initializable, Ownable {
     function initialize(
         address _owner, 
         address _rewardDelegatorsAddress, 
-        uint256 _rewardPerEpoch, 
+        bytes32[] memory _networkIds,
+        uint256[] memory _rewardWeight,
+        uint256 _totalRewardsPerEpoch, 
         address _PONDAddress,
         uint256 _payoutDenomination) 
         public
         initializer
     {
+        require(
+            _networkIds.length == _rewardWeight.length, 
+            "ClusterRewards:initialize - Each NetworkId need a corresponding RewardPerEpoch and vice versa"
+        );
         initialize(_owner);
+        uint256 weight = 0;
         rewardDelegatorsAddress = _rewardDelegatorsAddress;
-        rewardPerEpoch = _rewardPerEpoch;
+        for(uint256 i=0; i < _networkIds.length; i++) {
+            rewardWeight[_networkIds[i]] = _rewardWeight[i];
+            weight = weight.add(_rewardWeight[i]);
+        }
+        totalWeight = weight;
+        totalRewardsPerEpoch = _totalRewardsPerEpoch;
         POND = ERC20(_PONDAddress);
         payoutDenomination = _payoutDenomination;
     }
 
-    function feed(address[] memory _clusters, uint256[] memory _payouts) public onlyOwner {
+    function addNetwork(bytes32 _networkId, uint256 _rewardWeight) public onlyOwner {
+        require(rewardWeight[_networkId] == 0, "ClusterRewards:addNetwork - Network already exists");
+        require(_rewardWeight != 0, "ClusterRewards:addNetwork - Reward can't be 0");
+        rewardWeight[_networkId] = _rewardWeight;
+        totalWeight = totalWeight.add(_rewardWeight);
+        emit NetworkAdded(_networkId, _rewardWeight);
+    }
+
+    function removeNetwork(bytes32 _networkId) public onlyOwner {
+        uint256 networkWeight = rewardWeight[_networkId];
+        require( networkWeight != 0, "ClusterRewards:removeNetwork - Network doesn't exist");
+        delete rewardWeight[_networkId];
+        totalWeight = totalWeight.sub(networkWeight);
+        emit NetworkRemoved(_networkId);
+    }
+
+    function changeNetworkReward(bytes32 _networkId, uint256 _updatedRewardWeight) public onlyOwner {
+        uint256 networkWeight = rewardWeight[_networkId];
+        require( networkWeight != 0, "ClusterRewards:changeNetworkRewards - Network doesn't exists");
+        rewardWeight[_networkId] = _updatedRewardWeight;
+        totalWeight = totalWeight.sub(networkWeight).add(_updatedRewardWeight);
+        emit NetworkRewardUpdated(_networkId, _updatedRewardWeight);
+    }
+
+    function feed(bytes32 _networkId, address[] memory _clusters, uint256[] memory _payouts) public onlyOwner {
         for(uint256 i=0; i < _clusters.length; i++) {
             clusterRewards[_clusters[i]] = clusterRewards[_clusters[i]].add(
-                                                rewardPerEpoch.mul(_payouts[i]).div(payoutDenomination)
+                                                totalRewardsPerEpoch
+                                                .mul(rewardWeight[_networkId])
+                                                .mul(_payouts[i])
+                                                .div(totalWeight)
+                                                .div(payoutDenomination)
                                             );
         }
+        emit ClusterRewarded(_networkId);
+    }
+
+    function getRewardPerEpoch(bytes32 _networkId) public view returns(uint256) {
+        return totalRewardsPerEpoch.mul(rewardWeight[_networkId]).div(totalWeight);
     }
 
     // only cluster registry is necessary because the rewards 
