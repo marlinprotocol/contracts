d9b4bcadc85e7f42baea143a9163d1cfe4e455b9
whitewalker608 <nitingoel608@gmail.com>
2021-10-11 09:54:03 +0530

    unchained_init and uups proxy

--- contracts/Token/TokenLogic.sol
+++ contracts/Token/TokenLogic.sol
@@ -1,26 +1,49 @@
-pragma solidity 0.8.7;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/presets/ERC20PresetMinterPauserUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract TokenLogic is
     Initializable,
+    ContextUpgradeable,
+    IAccessControlUpgradeable,
+    IAccessControlEnumerableUpgradeable,
+    IERC165Upgradeable,
+    IERC20Upgradeable,
+    IERC20MetadataUpgradeable,
     ERC20Upgradeable,
     ERC20CappedUpgradeable,
     ERC20BurnableUpgradeable,
-    ERC20PresetMinterPauserUpgradeable
+    ERC20PresetMinterPauserUpgradeable,
+    ERC1967UpgradeUpgradeable,
+    UUPSUpgradeable,
+    OwnableUpgradeable
 {
     function initialize(
         string memory _name,
         string memory _symbol,
         address _bridge
     ) public initializer {
-        __ERC20PresetMinterPauser_init(_name, _symbol);
-        __ERC20Capped_init(10000000000e18);
+
+        __Context_init_unchained();
+        __ERC165_init_unchained();
+        __AccessControl_init_unchained();
+        __AccessControlEnumerable_init_unchained();
+        __ERC20_init_unchained(_name, _symbol);
+        __ERC20Capped_init_unchained(10000000000e18);
+        __ERC20Burnable_init_unchained();
+        __Pausable_init_unchained();
+        __ERC20Pausable_init_unchained();
+        __ERC20PresetMinterPauser_init_unchained(_name, _symbol);
+        __Ownable_init_unchained();
+
         ERC20PresetMinterPauserUpgradeable.mint(_bridge, 1000000000e18);
+        
     }
     
     function _mint(address account, uint256 amount) internal virtual override(ERC20Upgradeable, ERC20CappedUpgradeable) {
@@ -30,4 +53,6 @@ contract TokenLogic is
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20Upgradeable, ERC20PresetMinterPauserUpgradeable) {
         super._beforeTokenTransfer(from, to, amount);
     }
+    
+    function _authorizeUpgrade(address account) internal override onlyOwner{}
 }
