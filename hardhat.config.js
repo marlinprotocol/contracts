d4c786d70724d6a144b683fa04cd8f27f276d676
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-10-31 11:58:17 +0530

    initial hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    },
+  },
+};
+
