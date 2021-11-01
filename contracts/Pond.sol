8b84922f6627d120239efa3719839630a9daf326
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-11-01 11:00:27 +0530

    update pond

--- contracts/Pond.sol
+++ contracts/Pond.sol
@@ -1,6 +1,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/IAccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/IAccessControlEnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
@@ -9,27 +15,29 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract Pond is
-    Initializable,
-    ContextUpgradeable,
-    IAccessControlUpgradeable,
-    IAccessControlEnumerableUpgradeable,
-    IERC165Upgradeable,
-    IERC20Upgradeable,
-    IERC20MetadataUpgradeable,
-    ERC20Upgradeable,
-    ERC20CappedUpgradeable,
-    ERC20BurnableUpgradeable,
-    ERC20PresetMinterPauserUpgradeable,
-    ERC1967UpgradeUpgradeable,
-    UUPSUpgradeable,
-    OwnableUpgradeable
+    Initializable,  // initializer
+    ContextUpgradeable,  // _msgSender, _msgData
+    // IERC165Upgradeable,
+    // IAccessControlUpgradeable,
+    // IAccessControlEnumerableUpgradeable,
+    // IERC20Upgradeable,
+    // IERC20MetadataUpgradeable,
+    ERC165Upgradeable,  // supportsInterface
+    AccessControlUpgradeable,  // RBAC
+    AccessControlEnumerableUpgradeable,  // RBAC enumeration
+    ERC20Upgradeable,  // token
+    ERC20CappedUpgradeable,  // supply cap
+    ERC20BurnableUpgradeable,  // burn
+    PausableUpgradeable,  // _pause
+    ERC20PausableUpgradeable,  // check paused or not
+    ERC20PresetMinterPauserUpgradeable,  // mint, pause, unpause, MINTER_ROLE, PAUSER_ROLE
+    ERC1967UpgradeUpgradeable,  // delegate slots, proxy admin, private upgrade
+    UUPSUpgradeable  // public upgrade
 {
     function initialize(
         string memory _name,
-        string memory _symbol,
-        address _bridge
-    ) public initializer {
-
+        string memory _symbol
+    ) public initializer override {
         __Context_init_unchained();
         __ERC165_init_unchained();
         __AccessControl_init_unchained();
@@ -42,19 +50,37 @@ contract Pond is
         __ERC20PresetMinterPauser_init_unchained(_name, _symbol);
         __ERC1967Upgrade_init_unchained();
         __UUPSUpgradeable_init_unchained();
-        __Ownable_init_unchained();
+    }
 
-        ERC20PresetMinterPauserUpgradeable.mint(_bridge, 1000000000e18);
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165Upgradeable, AccessControlUpgradeable, AccessControlEnumerableUpgradeable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 
+    function _setupRole(bytes32 role, address account) internal virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
+        super._setupRole(role, account);
+    }
+
+    function grantRole(bytes32 role, address account) public virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
+        super.grantRole(role, account);
+    }
+
+    function revokeRole(bytes32 role, address account) public virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
+        super.revokeRole(role, account);
+    }
+
+    function renounceRole(bytes32 role, address account) public virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
+        super.renounceRole(role, account);
     }
 
     function _mint(address account, uint256 amount) internal virtual override(ERC20Upgradeable, ERC20CappedUpgradeable) {
         super._mint(account, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20Upgradeable, ERC20PresetMinterPauserUpgradeable) {
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20Upgradeable, ERC20PausableUpgradeable, ERC20PresetMinterPauserUpgradeable) {
         super._beforeTokenTransfer(from, to, amount);
     }
 
-    function _authorizeUpgrade(address account) internal override onlyOwner{}
+    function _authorizeUpgrade(address account) internal override {
+        require(hasRole(DEFAULT_ADMIN_ROLE, account), "Pond: must be admin to upgrade");
+    }
 }
