756da630f23ce0495b615a6eba41f57c3365d615
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-11-05 21:03:00 +0530

    sample tests

---
+++ test/MPond.ts
@@ -0,0 +1,23 @@
+const { ethers, upgrades } = require('hardhat');
+
+describe('MPond', function () {
+  it('deploys', async function () {
+    const MPond = await ethers.getContractFactory('MPond');
+    await MPond.deploy();
+  });
+});
+
+describe('MPond', function () {
+  it('deploys', async function () {
+    const MPond = await ethers.getContractFactory('MPond');
+    await MPond.deploy();
+  });
+});
+
+describe('MPond', function () {
+  it('deploys', async function () {
+    const MPond = await ethers.getContractFactory('MPond');
+    await MPond.deploy();
+  });
+});
+
