ef7dc4a2cbcb793ef406b3ca037afac61e0c4895
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-11-08 11:42:05 +0530

    set typehashes as constants

--- contracts/MPond.sol
+++ contracts/MPond.sol
@@ -357,17 +357,17 @@ contract MPond is
     uint256[48] private __gap4;
 
     /// @notice The EIP-712 typehash for the contract's domain
-    bytes32 public DOMAIN_TYPEHASH = keccak256(
+    bytes32 public constant DOMAIN_TYPEHASH = keccak256(
         "EIP712Domain(string name,uint256 chainId,address verifyingContract)"
     );
 
     /// @notice The EIP-712 typehash for the delegation struct used by the contract
-    bytes32 public DELEGATION_TYPEHASH = keccak256(
+    bytes32 public constant DELEGATION_TYPEHASH = keccak256(
         "Delegation(address delegatee,uint256 nonce,uint256 expiry,uint96 amount)"
     );
 
     /// @notice The EIP-712 typehash for the delegation struct used by the contract
-    bytes32 public UNDELEGATION_TYPEHASH = keccak256(
+    bytes32 public constant UNDELEGATION_TYPEHASH = keccak256(
         "Undelegation(address delegatee,uint256 nonce,uint256 expiry,uint96 amount)"
     );
 
