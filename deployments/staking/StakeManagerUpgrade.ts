752782418e123d99bc489a75d14cdbe6fa170d1f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-12-14 11:43:08 +0530

    StakeManager upgrade script

---
+++ deployments/staking/StakeManagerUpgrade.ts
@@ -0,0 +1,50 @@
+import { ethers, upgrades } from 'hardhat';
+import { BigNumber as BN, Signer, Contract } from 'ethers';
+import * as fs from 'fs';
+
+
+declare module 'ethers' {
+  interface BigNumber {
+    e18(this: BigNumber): BigNumber;
+  }
+}
+BN.prototype.e18 = function () {
+  return this.mul(BN.from(10).pow(18))
+}
+
+
+async function main() {
+  let chainId = (await ethers.provider.getNetwork()).chainId;
+  console.log("Chain Id:", chainId);
+
+  var addresses: {[key: string]: {[key: string]: string}} = {};
+  if(fs.existsSync('address.json')) {
+    addresses = JSON.parse(fs.readFileSync('address.json', 'utf8'));
+  }
+
+  if(addresses[chainId] === undefined ||
+     addresses[chainId]['StakeManager'] === undefined
+  ) {
+    console.log("Missing dependencies");
+    return;
+  }
+
+  let signers = await ethers.getSigners();
+  let addrs = await Promise.all(signers.map(a => a.getAddress()));
+
+  console.log("Signer addrs:", addrs);
+
+  const StakeManager = await ethers.getContractFactory('StakeManager');
+  let stakeManager = await upgrades.upgradeProxy(addresses[chainId]['StakeManager'], StakeManager, { kind: "uups" });
+
+  console.log("Deployed addr:", stakeManager.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
