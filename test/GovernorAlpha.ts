20301a680c90b24d29c8447d4bc7f8bbe08849f3
whitewalker608 <nitingoel608@gmail.com>
2022-02-28 14:05:31 +0530

    more governorAlpha tests relating to admin update

--- test/GovernorAlpha.ts
+++ test/GovernorAlpha.ts
@@ -319,6 +319,123 @@ describe('GovernorAlpha', function () {
     });
 });
 
+describe('GovernorAlpha', function () {
+  let signers: Signer[];
+  let addrs: string[];
+  let bridge: Contract;
+  let timelock: Contract;
+  let governorAlpha: Contract;
+  let mpond: Contract;
+  beforeEach(async function () {
+      signers = await ethers.getSigners();
+      addrs = await Promise.all(signers.map(a => a.getAddress()));
+
+      const MPond = await ethers.getContractFactory('MPond');
+      mpond = await upgrades.deployProxy(MPond, { kind: "uups" });
+      
+
+      const Pond = await ethers.getContractFactory('Pond');
+      const pond = await upgrades.deployProxy(Pond, ["Marlin POND", "POND"],{ kind: "uups" });
+
+      const Timelock = await ethers.getContractFactory('Timelock');
+      timelock = await upgrades.deployProxy(Timelock, [2*24*60*60],{ kind: "uups" });
+      
+      const Bridge = await ethers.getContractFactory('Bridge');
+      bridge = await upgrades.deployProxy(Bridge,[mpond.address, pond.address, timelock.address] , { kind: "uups" }); // nitin check changing timelock address to addrs[1]
+
+      const GovernorAlpha = await ethers.getContractFactory('GovernorAlpha');
+      governorAlpha = await upgrades.deployProxy(GovernorAlpha, [timelock.address, mpond.address, addrs[1]] , { kind: "uups" });
+      await timelock.grantRole(await timelock.DEFAULT_ADMIN_ROLE(), governorAlpha.address); // nitin: check if this is expected
+      await timelock.revokeRole(await timelock.DEFAULT_ADMIN_ROLE(), addrs[0]); // nitin: should we add a check for this case in the contract
+
+  });
+
+  it('non guardian cannot queue setTimelockPendingAdmin', async() => {
+    let currentBlockTimestamp = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+    let eta = currentBlockTimestamp + 2*24*60*60 + 1;
+    await expect(governorAlpha.__queueSetTimelockPendingAdmin(addrs[2], eta)).to.be.reverted;
+  });
+
+  it('guardian can queue setTimelockPendingAdmin', async() => {
+    let currentBlockTimestamp = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+    let eta = currentBlockTimestamp + 2*24*60*60 + 1;
+    await governorAlpha.connect(signers[1]).__queueSetTimelockPendingAdmin(addrs[2], eta);
+    let calldata =
+            ethers.utils.defaultAbiCoder.encode(
+              ["address"],
+              [addrs[2]]
+            );
+    let target = timelock.address;
+    let value = 0;
+    let signature = "setPendingAdmin(address)";
+    let txhash = await ethers.utils.keccak256(
+            ethers.utils.defaultAbiCoder.encode(
+                ["address", "uint256", "string", "bytes", "uint256"], 
+                [target, value, signature, calldata, eta]
+      ));
+    expect(await timelock.queuedTransactions(txhash)).to.be.true;
+  });
+
+  it('non guardian cannot execute setTimelockPendingAdmin', async() => {
+    let currentBlockTimestamp = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+    let eta = currentBlockTimestamp + 2*24*60*60 + 1;
+    await governorAlpha.connect(signers[1]).__queueSetTimelockPendingAdmin(addrs[2], eta);
+    await expect(governorAlpha.__executeSetTimelockPendingAdmin(addrs[2], eta)).to.be.reverted;
+  });
+
+  it('guardian can execute setTimelockPendingAdmin', async() => {
+    let currentBlockTimestamp = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+    let eta = currentBlockTimestamp + 2*24*60*60 + 1;
+    await governorAlpha.connect(signers[1]).__queueSetTimelockPendingAdmin(addrs[2], eta);
+    await skipTime(eta - currentBlockTimestamp);
+    await governorAlpha.connect(signers[1]).__executeSetTimelockPendingAdmin(addrs[2], eta);
+    expect(await timelock.pendingAdmin()).to.equal(addrs[2]);
+  });
+
+  it('non guardian cannot call accept admin', async() => {
+    const GovernorAlpha = await ethers.getContractFactory('GovernorAlpha');
+    let governorAlpha2 = await upgrades.deployProxy(GovernorAlpha, [timelock.address, mpond.address, addrs[2]] , { kind: "uups" });
+
+    let currentBlockTimestamp = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+    let eta = currentBlockTimestamp + 2*24*60*60 + 1;
+    await governorAlpha.connect(signers[1]).__queueSetTimelockPendingAdmin(governorAlpha2.address, eta);
+    await skipTime(eta - currentBlockTimestamp);
+    await governorAlpha.connect(signers[1]).__executeSetTimelockPendingAdmin(governorAlpha2.address, eta);
+    expect(await timelock.pendingAdmin()).to.equal(governorAlpha2.address);
+    await expect(governorAlpha2.__acceptAdmin()).to.be.reverted;
+  });
+
+  it('guardian can call accept admin', async() => {
+    const GovernorAlpha = await ethers.getContractFactory('GovernorAlpha');
+    let governorAlpha2 = await upgrades.deployProxy(GovernorAlpha, [timelock.address, mpond.address, addrs[2]] , { kind: "uups" });
+
+    let currentBlockTimestamp = (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;
+    let eta = currentBlockTimestamp + 2*24*60*60 + 1;
+    await governorAlpha.connect(signers[1]).__queueSetTimelockPendingAdmin(governorAlpha2.address, eta);
+    await skipTime(eta - currentBlockTimestamp);
+    await governorAlpha.connect(signers[1]).__executeSetTimelockPendingAdmin(governorAlpha2.address, eta);
+    expect(await timelock.pendingAdmin()).to.equal(governorAlpha2.address);
+    await governorAlpha2.connect(signers[2]).__acceptAdmin();
+    expect(await timelock.hasRole(timelock.DEFAULT_ADMIN_ROLE(), governorAlpha2.address)).to.be.true;
+    expect(await timelock.hasRole(timelock.DEFAULT_ADMIN_ROLE(), governorAlpha.address)).to.be.false;
+  });
+
+  it('non guardian cannot abdicate', async() => {
+    await expect(governorAlpha.__abdicate()).to.be.reverted;
+  });
+  
+  it('guardian can abdicate', async() => {
+    await governorAlpha.connect(signers[1]).__abdicate();
+    expect(await governorAlpha.guardian()).to.equal('0x0000000000000000000000000000000000000000');
+  });
+
+});
+
+async function skipTime(t: number) {
+  await ethers.provider.send('evm_increaseTime', [t]);
+  await skipBlocks(1);
+}
+
 async function skipBlocks(n: number) {
     await Promise.all([...Array(n)].map(async x => await ethers.provider.send('evm_mine', [])));
 }
