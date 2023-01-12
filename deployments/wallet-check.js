f3bff217a7552bc2e0badddb6caa74797d44d623
carrot <carrot@example.com>
2023-01-12 10:48:01 +0000

    remove unnecessary files

--- deployments/wallet-check.js
+++
@@ -1,7 +0,0 @@
-const Web3 = require("web3");
-const networks = require("./networks");
-const web3 = new Web3(networks[process.env.NETWORK]);
-
-web3.eth.accounts.wallet.add(process.env.PRIV_KEY);
-
-console.log(web3.eth.accounts.wallet[0].address);
--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-
-module.exports = function (deployer, network, accounts) {
-  if (network == "development") {
-    deployer.deploy(Migrations);
-  }
-};
