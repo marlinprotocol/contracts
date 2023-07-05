0a306ffc4fa9ec839c4a791b2427e59056cb3676
Akshay Meher <akshay_111meher@hotmail.com>
2023-07-05 13:17:05 +0400

    fix lints

--- contracts/staking/ClusterRewards.sol
+++ contracts/staking/ClusterRewards.sol
@@ -15,14 +15,14 @@ import "./ReceiverStaking.sol";
 import "./interfaces/IClusterRewards.sol";
 
 contract ClusterRewards is
-    Initializable, // initializer
-    ContextUpgradeable, // _msgSender, _msgData
-    ERC165Upgradeable, // supportsInterface
-    AccessControlUpgradeable, // RBAC
-    AccessControlEnumerableUpgradeable, // RBAC enumeration
-    ERC1967UpgradeUpgradeable, // delegate slots, proxy admin, private upgrade
-    UUPSUpgradeable, // public upgrade
-    IClusterRewards // interface
+    Initializable,  // initializer
+    ContextUpgradeable,  // _msgSender, _msgData
+    ERC165Upgradeable,  // supportsInterface
+    AccessControlUpgradeable,  // RBAC
+    AccessControlEnumerableUpgradeable,  // RBAC enumeration
+    ERC1967UpgradeUpgradeable,  // delegate slots, proxy admin, private upgrade
+    UUPSUpgradeable,  // public upgrade
+    IClusterRewards  // interface
 {
     // in case we add more contracts in the inheritance chain
     uint256[500] private __gap0;
@@ -37,36 +37,28 @@ contract ClusterRewards is
         _;
     }
 
-    //-------------------------------- Overrides start --------------------------------//
+//-------------------------------- Overrides start --------------------------------//
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view virtual override(ERC165Upgradeable, AccessControlUpgradeable, AccessControlEnumerableUpgradeable) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165Upgradeable, AccessControlUpgradeable, AccessControlEnumerableUpgradeable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function _grantRole(
-        bytes32 role,
-        address account
-    ) internal virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
+    function _grantRole(bytes32 role, address account) internal virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
         super._grantRole(role, account);
     }
 
-    function _revokeRole(
-        bytes32 role,
-        address account
-    ) internal virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
+    function _revokeRole(bytes32 role, address account) internal virtual override(AccessControlUpgradeable, AccessControlEnumerableUpgradeable) {
         super._revokeRole(role, account);
 
         // protect against accidentally removing all admins
         require(getRoleMemberCount(DEFAULT_ADMIN_ROLE) != 0, "Cannot be adminless");
     }
 
-    function _authorizeUpgrade(address /*account*/) internal view override onlyAdmin {}
+    function _authorizeUpgrade(address /*account*/) onlyAdmin internal view override {}
 
-    //-------------------------------- Overrides end --------------------------------//
+//-------------------------------- Overrides end --------------------------------//
 
-    //-------------------------------- Initializer start --------------------------------//
+//-------------------------------- Initializer start --------------------------------//
 
     uint256[50] private __gap1;
 
@@ -78,9 +70,18 @@ contract ClusterRewards is
         uint256[] memory _rewardWeight,
         address[] memory _clusterSelectors,
         uint256 _totalRewardsPerEpoch
-    ) public initializer {
-        require(_networkIds.length == _rewardWeight.length, "CRW:I-Each NetworkId need a corresponding RewardPerEpoch and vice versa");
-        require(_networkIds.length == _clusterSelectors.length, "CRW:I-Each NetworkId need a corresponding clusterSelector and vice versa");
+    )
+        public
+        initializer
+    {
+        require(
+            _networkIds.length == _rewardWeight.length,
+            "CRW:I-Each NetworkId need a corresponding RewardPerEpoch and vice versa"
+        );
+        require(
+            _networkIds.length == _clusterSelectors.length,
+            "CRW:I-Each NetworkId need a corresponding clusterSelector and vice versa"
+        );
 
         __Context_init_unchained();
         __ERC165_init_unchained();
@@ -96,9 +97,9 @@ contract ClusterRewards is
         _updateReceiverStaking(_receiverStaking);
 
         uint256 _weight = 0;
-        for (uint256 i = 0; i < _networkIds.length; i++) {
+        for(uint256 i=0; i < _networkIds.length; i++) {
             rewardWeight[_networkIds[i]] = _rewardWeight[i];
-            require(_clusterSelectors[i] != address(0), "CRW:CN-ClusterSelector must exist");
+            require(_clusterSelectors[i] !=  address(0), "CRW:CN-ClusterSelector must exist");
             clusterSelectors[_networkIds[i]] = IClusterSelector(_clusterSelectors[i]);
             _weight += _rewardWeight[i];
             emit NetworkAdded(_networkIds[i], _rewardWeight[i], _clusterSelectors[i]);
@@ -107,18 +108,19 @@ contract ClusterRewards is
         _changeRewardPerEpoch(_totalRewardsPerEpoch);
     }
 
-    //-------------------------------- Initializer end --------------------------------//
+//-------------------------------- Initializer end --------------------------------//
 
-    //-------------------------------- Admin functions start --------------------------------//
+//-------------------------------- Admin functions start --------------------------------//
 
     bytes32 public constant CLAIMER_ROLE = keccak256("CLAIMER_ROLE");
     uint256 public constant RECEIVER_TICKETS_PER_EPOCH = type(uint16).max;
+    uint256 constant SWITCHING_PERIOD = 33 days;
 
-    mapping(address => uint256) public override clusterRewards;
+    mapping(address => uint256) public clusterRewards;
 
-    mapping(bytes32 => uint256) public override rewardWeight;
+    mapping(bytes32 => uint256) public rewardWeight;
     uint256 public totalRewardWeight;
-    uint256 public override totalRewardsPerEpoch;
+    uint256 public totalRewardsPerEpoch;
     uint256 public __unused_payoutDenomination;
 
     mapping(uint256 => uint256) public __unused_rewardDistributedPerEpoch;
@@ -126,7 +128,7 @@ contract ClusterRewards is
     uint256 public __unused_rewardDistributionWaitTime;
 
     mapping(address => mapping(uint256 => uint256)) public ticketsIssued;
-    mapping(bytes32 => IClusterSelector) public override clusterSelectors; // networkId -> clusterSelector
+    mapping(bytes32 => IClusterSelector) public clusterSelectors; // networkId -> clusterSelector
     ReceiverStaking public receiverStaking;
 
     event NetworkAdded(bytes32 networkId, uint256 rewardPerEpoch, address clusterSelector);
@@ -138,9 +140,9 @@ contract ClusterRewards is
     event RewardDistributionWaitTimeChanged(uint256 updatedWaitTime);
     event TicketsIssued(bytes32 indexed networkId, uint256 indexed epoch, address indexed user);
 
-    function addNetwork(bytes32 _networkId, uint256 _rewardWeight, address _clusterSelector) external override onlyAdmin {
+    function addNetwork(bytes32 _networkId, uint256 _rewardWeight, address _clusterSelector) external onlyAdmin {
         require(rewardWeight[_networkId] == 0, "CRW:AN-Network already exists");
-        require(_clusterSelector != address(0), "CRW:AN-ClusterSelector must exist");
+        require(_clusterSelector !=  address(0), "CRW:AN-ClusterSelector must exist");
         rewardWeight[_networkId] = _rewardWeight;
         IClusterSelector networkClusterSelector = IClusterSelector(_clusterSelector);
         require(networkClusterSelector.START_TIME() == receiverStaking.START_TIME(), "CRW:AN-start time inconsistent");
@@ -151,7 +153,7 @@ contract ClusterRewards is
         emit NetworkAdded(_networkId, _rewardWeight, _clusterSelector);
     }
 
-    function removeNetwork(bytes32 _networkId) external override onlyAdmin {
+    function removeNetwork(bytes32 _networkId) external onlyAdmin {
         uint256 networkWeight = rewardWeight[_networkId];
         require(address(clusterSelectors[_networkId]) != address(0), "CRW:RN-Network doesnt exist");
         delete rewardWeight[_networkId];
@@ -160,13 +162,13 @@ contract ClusterRewards is
         emit NetworkRemoved(_networkId);
     }
 
-    function updateNetwork(bytes32 _networkId, uint256 _updatedRewardWeight, address _updatedClusterSelector) external override onlyAdmin {
+    function updateNetwork(bytes32 _networkId, uint256 _updatedRewardWeight, address _updatedClusterSelector) external onlyAdmin {
         uint256 networkWeight = rewardWeight[_networkId];
-        require(_updatedClusterSelector != address(0), "CRW:UN-ClusterSelector must exist");
+        require(_updatedClusterSelector !=  address(0), "CRW:UN-ClusterSelector must exist");
         address currentClusterSelector = address(clusterSelectors[_networkId]);
         require(currentClusterSelector != address(0), "CRW:UN-Network doesnt exist");
 
-        if (_updatedClusterSelector != currentClusterSelector) {
+        if(_updatedClusterSelector != currentClusterSelector) {
             IClusterSelector networkClusterSelector = IClusterSelector(_updatedClusterSelector);
             require(networkClusterSelector.START_TIME() == receiverStaking.START_TIME(), "CRW:UN-start time inconsistent");
             require(networkClusterSelector.EPOCH_LENGTH() == receiverStaking.EPOCH_LENGTH(), "CRW:UN-epoch length inconsistent");
@@ -188,7 +190,7 @@ contract ClusterRewards is
         emit ReceiverStakingUpdated(_receiverStaking);
     }
 
-    function changeRewardPerEpoch(uint256 _updatedRewardPerEpoch) external override onlyAdmin {
+    function changeRewardPerEpoch(uint256 _updatedRewardPerEpoch) external onlyAdmin {
         _changeRewardPerEpoch(_updatedRewardPerEpoch);
     }
 
@@ -197,7 +199,9 @@ contract ClusterRewards is
         emit RewardPerEpochChanged(_updatedRewardPerEpoch);
     }
 
-    //-------------------------------- Admin functions end --------------------------------//
+//-------------------------------- Admin functions end --------------------------------//
+
+//-------------------------------- User functions start --------------------------------//
 
     function _getRewardShare(
         uint256 _totalNetworkRewardsPerEpoch,
@@ -210,13 +214,7 @@ contract ClusterRewards is
         }
     }
 
-    function _processReceiverTickets(
-        address _receiver,
-        uint256 _epoch,
-        address[] memory _selectedClusters,
-        uint16[] memory _tickets,
-        uint256 _rewardShare
-    ) internal {
+    function _processReceiverTickets(address _receiver, uint256 _epoch, address[] memory _selectedClusters, uint16[] memory _tickets, uint256 _rewardShare) internal {
         require(!_isTicketsIssued(_receiver, _epoch), "CRW:IPRT-Tickets already issued");
         unchecked {
             require(_selectedClusters.length <= _tickets.length + 1, "CRW:IPRT-Tickets length not matching selected clusters");
@@ -240,25 +238,25 @@ contract ClusterRewards is
         _markAsIssued(_receiver, _epoch);
     }
 
-    function _isTicketsIssued(address _receiver, uint256 _epoch) internal view returns (bool) {
+    function _isTicketsIssued(address _receiver, uint256 _epoch) internal view returns(bool) {
         unchecked {
-            uint256 _index = _epoch / 256;
-            uint256 _pos = _epoch % 256;
+            uint256 _index = _epoch/256;
+            uint256 _pos = _epoch%256;
             uint256 _issuedFlags = ticketsIssued[_receiver][_index];
-            return (_issuedFlags & (2 ** (255 - _pos))) != 0;
+            return (_issuedFlags & 2**(255-_pos)) != 0;
         }
     }
 
-    function isTicketsIssued(address _receiver, uint256 _epoch) public view returns (bool) {
+    function isTicketsIssued(address _receiver, uint256 _epoch) public view returns(bool) {
         return _isTicketsIssued(_receiver, _epoch);
     }
 
     function _markAsIssued(address _receiver, uint256 _epoch) internal {
         unchecked {
-            uint256 _index = _epoch / 256;
-            uint256 _pos = _epoch % 256;
+            uint256 _index = _epoch/256;
+            uint256 _pos = _epoch%256;
             uint256 _issuedFlags = ticketsIssued[_receiver][_index];
-            ticketsIssued[_receiver][_index] = _issuedFlags | (2 ** (255 - _pos));
+            ticketsIssued[_receiver][_index] = _issuedFlags | 2**(255-_pos);
         }
     }
 
@@ -325,62 +323,59 @@ contract ClusterRewards is
         receiverRewardPayment[_receiver].rewardRemaining = receiverPayment.rewardRemaining;
     }
 
-    function _parseTicketInfo(
-        bytes memory ticketInfo
-    ) internal view returns (bytes32 networkId, uint256 fromEpoch, uint256 noOfEpochs, uint16[][] memory tickets) {
+    function _parseTicketInfo(bytes memory ticketInfo) internal view returns(
+        bytes32 networkId,
+        uint256 fromEpoch,
+        uint256 noOfEpochs,
+        uint16[][] memory tickets
+    ) {
         // Ticket Structure
         // |--NetworkId(256 bits)--|--FromEpoch(32 bits)--|--N*Ticket(16 bits)--|
 
         uint256 length;
         bytes32 currentWord;
         assembly {
-            length := mload(ticketInfo)
+            length :=  mload(ticketInfo)
             networkId := mload(add(ticketInfo, 0x20))
             currentWord := mload(add(ticketInfo, 0x40))
         }
         fromEpoch = uint256(currentWord >> 224);
         unchecked {
-            require(length >= 36 && (length - 36) % 8 == 0, "CR:IPTI-invalid ticket info encoding");
-            noOfEpochs = (length - 36) / 8; // 32 (networkId) + 4 (fromEpoch) / 2(tickets) / 4(tickets per epoch)
+            require(length >= 36 && (length - 36)%8 == 0, "CR:IPTI-invalid ticket info encoding");
+            noOfEpochs = (length - 36)/8; // 32 (networkId) + 4 (fromEpoch) / 2(tickets) / 4(tickets per epoch)
             // +1 because of slight overflow on last word
-            uint256 noOfWords = length / 32;
+            uint256 noOfWords = length/32;
             tickets = new uint16[][](noOfEpochs);
             uint256 clustersToSelect = clusterSelectors[networkId].NUMBER_OF_CLUSTERS_TO_SELECT();
             // revert due to memory expansion overflow in case of underflow
             tickets[0] = new uint16[](clustersToSelect - 1);
             (uint256 _currentEpochIndex, uint256 _currentTicketIndex) = _extractTickets(currentWord, 2, 0, 0, tickets);
 
-            for (uint256 i = 1; i < noOfWords; ++i) {
+            for(uint256 i=1; i < noOfWords; ++i) {
                 assembly {
                     currentWord := mload(add(ticketInfo, add(0x40, mul(0x20, i))))
                 }
-                (_currentEpochIndex, _currentTicketIndex) = _extractTickets(
-                    currentWord,
-                    0,
-                    _currentEpochIndex,
-                    _currentTicketIndex,
-                    tickets
-                );
+                (_currentEpochIndex, _currentTicketIndex) = _extractTickets(currentWord, 0, _currentEpochIndex, _currentTicketIndex, tickets);
             }
         }
     }
 
     function _extractTickets(
-        bytes32 word,
-        uint256 startIndex,
-        uint256 currentEpochIndex,
+        bytes32 word, 
+        uint256 startIndex, 
+        uint256 currentEpochIndex, 
         uint256 currentTicketIndex,
         uint16[][] memory tickets
-    ) internal pure returns (uint256, uint256) {
+    ) internal pure returns(uint256, uint256) {
         unchecked {
-            for (uint256 i = startIndex; i < 16; ++i) {
-                tickets[currentEpochIndex][currentTicketIndex] = uint16(uint256(word >> (256 - (i + 1) * 16)));
+            for(uint256 i = startIndex; i < 16; ++i) {
+                tickets[currentEpochIndex][currentTicketIndex] = uint16(uint256(word >> (256 - (i + 1)*16)));
                 currentTicketIndex++;
 
-                if (currentTicketIndex > tickets[currentEpochIndex].length - 1) {
-                    if (currentEpochIndex == tickets.length - 1) return (currentEpochIndex, currentTicketIndex);
+                if(currentTicketIndex > tickets[currentEpochIndex].length - 1) {
+                    if(currentEpochIndex == tickets.length - 1) return (currentEpochIndex, currentTicketIndex);
                     currentEpochIndex++;
-                    tickets[currentEpochIndex] = new uint16[](tickets[currentEpochIndex - 1].length);
+                    tickets[currentEpochIndex] = new uint16[](tickets[currentEpochIndex-1].length);
                     currentTicketIndex = 0;
                 }
             }
@@ -410,9 +405,9 @@ contract ClusterRewards is
         receiverRewardPayment[_receiver].rewardRemaining = receiverPayment.rewardRemaining;
     }
 
-    function claimReward(address _cluster) external onlyRole(CLAIMER_ROLE) override returns (uint256) {
+    function claimReward(address _cluster) external onlyRole(CLAIMER_ROLE) returns(uint256) {
         uint256 pendingRewards = clusterRewards[_cluster];
-        if (pendingRewards > 1) {
+        if(pendingRewards > 1) {
             uint256 rewardsToTransfer = pendingRewards - 1;
             clusterRewards[_cluster] = 1;
             return rewardsToTransfer;
@@ -424,8 +419,8 @@ contract ClusterRewards is
         return (totalRewardsPerEpoch * rewardWeight[_networkId]) / totalRewardWeight;
     }
 
-    //-------------------------------- User functions end --------------------------------//
-
+//-------------------------------- User functions end --------------------------------//
+     
     bytes32 public constant RECEIVER_PAYMENTS_MANAGER = keccak256("RECEIVER_PAYMENTS_MANAGER");
 
     mapping(address => ReceiverPayment) public receiverRewardPayment;
