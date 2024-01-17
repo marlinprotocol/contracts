b94aeb80443c0c6fea4534cada307733f720e8a4
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-01-17 12:00:09 +0530

    formatting

--- contracts/enclaves/interfaces/IAttestationVerifier.sol
+++ contracts/enclaves/interfaces/IAttestationVerifier.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 interface IAttestationVerifier {
     function verifyUnsafe(
         bytes memory attestation,
-        bytes memory  enclaveKey,
+        bytes memory enclaveKey,
         bytes memory PCR0,
         bytes memory PCR1,
         bytes memory PCR2,
@@ -14,7 +14,7 @@ interface IAttestationVerifier {
     ) external view returns(bool);
     function verify(
         bytes memory attestation,
-        bytes memory  enclaveKey,
+        bytes memory enclaveKey,
         bytes memory PCR0,
         bytes memory PCR1,
         bytes memory PCR2,
