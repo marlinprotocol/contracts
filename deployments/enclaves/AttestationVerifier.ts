5c517cd6c348110f3ecbe832a3d9fca8c8d3332f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-01-17 18:07:37 +0530

    lint

--- deployments/enclaves/AttestationVerifier.ts
+++ deployments/enclaves/AttestationVerifier.ts
@@ -12,52 +12,52 @@ export async function deploy(enclaveImages?: AttestationVerifier.EnclaveImageStr
 
     const AttestationVerifier = await ethers.getContractFactory('AttestationVerifier');
 
-    var addresses: {[key: string]: {[key: string]: string}} = {};
-    if(!noLog) {
+    var addresses: { [key: string]: { [key: string]: string } } = {};
+    if (!noLog) {
         console.log("Chain Id:", chainId);
 
-        if(fs.existsSync('address.json')) {
+        if (fs.existsSync('address.json')) {
             addresses = JSON.parse(fs.readFileSync('address.json', 'utf8'));
         }
-    
-        if(addresses[chainId] === undefined) {
+
+        if (addresses[chainId] === undefined) {
             addresses[chainId] = {};
         }
-    
-        if(addresses[chainId]['AttestationVerifier'] !== undefined) {
+
+        if (addresses[chainId]['AttestationVerifier'] !== undefined) {
             console.log("Existing deployment:", addresses[chainId]['AttestationVerifier']);
             return AttestationVerifier.attach(addresses[chainId]['AttestationVerifier']);
         }
     }
 
-    if(enclaveImages === undefined) {
+    if (enclaveImages === undefined) {
         enclaveImages = [];
-        for(let i = 0; i < chainConfig.enclaves.whitelistedImages.length; i++) {
-            if(chainConfig.enclaves.whitelistedImages[i].PCR === undefined) {
+        for (let i = 0; i < chainConfig.enclaves.whitelistedImages.length; i++) {
+            if (chainConfig.enclaves.whitelistedImages[i].PCR === undefined) {
                 throw new Error(`Image ${i}: PCR not defined for image`);
             }
-            if(chainConfig.enclaves.whitelistedImages[i].PCR.PCR0.length !== 96) {
+            if (chainConfig.enclaves.whitelistedImages[i].PCR.PCR0.length !== 96) {
                 throw new Error(`Image ${i}: PCR0 length is not 96`);
             }
-            if(chainConfig.enclaves.whitelistedImages[i].PCR.PCR1.length !== 96) {
+            if (chainConfig.enclaves.whitelistedImages[i].PCR.PCR1.length !== 96) {
                 throw new Error(`Image ${i}: PCR1 length is not 96`);
             }
-            if(chainConfig.enclaves.whitelistedImages[i].PCR.PCR2.length !== 96) {
+            if (chainConfig.enclaves.whitelistedImages[i].PCR.PCR2.length !== 96) {
                 throw new Error(`Image ${i}: PCR2 length is not 96`);
             }
             const image = {
-                PCR0: "0x"+chainConfig.enclaves.whitelistedImages[i].PCR.PCR0,
-                PCR1: "0x"+chainConfig.enclaves.whitelistedImages[i].PCR.PCR1,
-                PCR2: "0x"+chainConfig.enclaves.whitelistedImages[i].PCR.PCR2,
+                PCR0: "0x" + chainConfig.enclaves.whitelistedImages[i].PCR.PCR0,
+                PCR1: "0x" + chainConfig.enclaves.whitelistedImages[i].PCR.PCR1,
+                PCR2: "0x" + chainConfig.enclaves.whitelistedImages[i].PCR.PCR2,
             };
             enclaveImages.push(image);
         }
     }
 
-    if(enclaveKeys === undefined) {
+    if (enclaveKeys === undefined) {
         enclaveKeys = [];
-        for(let i = 0; i < chainConfig.enclaves.whitelistedImages.length; i++) {
-            if(chainConfig.enclaves.whitelistedImages[i].enclaveKey === undefined) {
+        for (let i = 0; i < chainConfig.enclaves.whitelistedImages.length; i++) {
+            if (chainConfig.enclaves.whitelistedImages[i].enclaveKey === undefined) {
                 throw new Error(`Image ${i}: Enclave key not defined for image`);
             }
             enclaveKeys.push(chainConfig.enclaves.whitelistedImages[i].enclaveKey);
@@ -66,7 +66,7 @@ export async function deploy(enclaveImages?: AttestationVerifier.EnclaveImageStr
 
     let attestationVerifier = await upgrades.deployProxy(AttestationVerifier, [enclaveImages, enclaveKeys], { kind: "uups" });
 
-    if(!noLog) {
+    if (!noLog) {
         console.log("Deployed addr:", attestationVerifier.address);
 
         addresses[chainId]['AttestationVerifier'] = attestationVerifier.address;
@@ -85,12 +85,12 @@ export async function verify() {
     let chainId = (await ethers.provider.getNetwork()).chainId;
     console.log("Chain Id:", chainId);
 
-    var addresses: {[key: string]: {[key: string]: string}} = {};
-    if(fs.existsSync('address.json')) {
+    var addresses: { [key: string]: { [key: string]: string } } = {};
+    if (fs.existsSync('address.json')) {
         addresses = JSON.parse(fs.readFileSync('address.json', 'utf8'));
     }
 
-    if(addresses[chainId] === undefined || addresses[chainId]['AttestationVerifier'] === undefined) {
+    if (addresses[chainId] === undefined || addresses[chainId]['AttestationVerifier'] === undefined) {
         throw new Error("Attestation Verifier not deployed");
     }
 
