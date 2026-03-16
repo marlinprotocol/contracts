d0a21c0a84ed833326ed51f568cc4a2084afaa81
Prateek <prateekreddyy@gmail.com>
2026-03-16 17:33:24 +0700

    fix: merge error

--- contracts/enclaves/MarketV1.sol
+++ contracts/enclaves/MarketV1.sol
@@ -287,11 +287,7 @@ contract MarketV1 is
         _jobReviseRate(jobId, _rate);
     }
 
-    function 
-    
-    
-    
-    (bytes32 _jobId, uint256 _rate) internal returns (bool isBalanceEnough) {
+    function _jobSettle(bytes32 _jobId, uint256 _rate) internal returns (bool isBalanceEnough) {
         uint256 lastSettled = jobs[_jobId].lastSettled;
 
         if (block.timestamp <= lastSettled) {
